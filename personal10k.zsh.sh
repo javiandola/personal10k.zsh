c617928dc0408fa0ad64a4688288154a1c4f5d02
javiandola <96593664+javiandola@users.noreply.github.com>
2022-01-11 21:50:55 +0100

    Add files via upload

---
+++ personal10k.zsh.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#Colores
+verde="\e[0;32m\033[1m"
+rojo="\e[0;31m\033[1m"
+azul="\e[0;34m\033[1m"
+amarillo="\e[0;33m\033[1m"
+morado="\e[0;35m\033[1m"
+turquesa="\e[0;36m\033[1m"
+gris="\e[0;37m\033[1m"
+fin="\033[0m\e[0m"
+
+ruta=$(pwd)
+
+#instalar powerlevel10k en la ruta del usuario
+usuario=$(whoami)
+cd /home/$usuario/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+clear
+echo -e "${amarillo}La configuracion del${fin} ${verde}$usuario${fin} ${amarillo}va a empezar en 5 segundos...${fin}"
+sleep 5
+zsh
+
+#Sustituir el archivo de configuracion
+rm -f .p10k.zsh
+cp $ruta/.p10k.zsh_user /home/$usuario/.p10k.zsh
+
+
+#instalar powerlevel10k en la ruta root
+sudo su
+cd /root/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+clear
+echo -e "${amarillo}La configuracion del${fin} ${verde}root${fin} ${amarillo}va a empezar en 5 segundos...${fin}"
+sleep 5
+zsh
+
+#Sustituir el archivo de configuracion
+rm -f .p10k.zsh
+cp $ruta/.p10k.zsh_root /root/.p10k.zsh
+
+clear
+echo -e "${amarillo}Se a completado la configuracion${fin}"
+slepp 1
+echo -e "${amarillo}Reinicie la terminal${fin}"
+exit
+
