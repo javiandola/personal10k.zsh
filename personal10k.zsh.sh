d60d45532dea554530f60b2d5b70f63084e7bfad
javiandola <96593664+javiandola@users.noreply.github.com>
2022-01-14 23:47:02 +0100

    Update personal10k.zsh.sh

--- personal10k.zsh.sh
+++ personal10k.zsh.sh
@@ -12,6 +12,7 @@ fin="\033[0m\e[0m"
 
 path=$(readlink -e $0)
 ruta=$(dirname $path)
+usuario=${SUDO_USER:-$USER}
 
 #instalar powerlevel10k en la ruta del usuario
 usuario=$(whoami)
@@ -29,4 +30,33 @@ cp $ruta/.p10k.zsh_user /home/$usuario/.p10k.zsh
 
 
 #instalar powerlevel10k en la ruta root
-sudo bash $ruta/personal10k.zsh_root.sh
+cd /root/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+clear
+echo -e "${amarillo}La configuracion del${fin} ${verde}root${fin} ${amarillo}va a empezar en 5 segundos...${fin}"
+sleep 5
+zsh
+
+#Sustituir el archivo de configuracion
+rm -f .p10k.zsh
+cp $ruta/.p10k.zsh_root /root/.p10k.zsh
+
+#Enlace simbolico del .zshrc
+ln -s -f /home/$usuario/.zshrc /root/.zshrc
+
+#Arreglar problema de permisos
+chown $usuario:$usuario /root
+chown $usuario:$usuario /root/.cache -R
+chown $usuario:$usuario /root/.local -R
+
+#Estableciendo zsh como shell predeterminada
+usermod --shell /usr/bin/zsh $usuario
+usermod --shell /usr/bin/zsh root
+
+#Saliendo
+clear
+echo -e "${amarillo}Se a completado la configuracion${fin}"
+slep 1
+echo -e "${amarillo}Reinicie la terminal${fin}"
+exit
